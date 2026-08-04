a49f0dcf814b85ca4c93dda488b08af14de20494
mehrzadpourghasemee-art <mehrzadpourghasemee@gmail.com>
2026-08-04 01:11:03 -1200

    Delete alphasite.js

--- alphasite.js
+++
@@ -1,9 +0,0 @@
-function login() {
-    const password = 
-    document.getElementById("pass").value;
-    if (password === "irankoodak") {
-        window.location.href = "home.html";
-    } else {
-        alert("رمز عبور صحیح نیست")
-    }
-}
